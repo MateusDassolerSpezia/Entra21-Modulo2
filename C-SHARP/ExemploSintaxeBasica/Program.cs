23d373dd6ce9a9933324529c23b200dff0c49461
MateusDassolerSpezia <mdspezia@furb.br>
2025-07-10 01:15:08 -0300

    Testes C#
    Variáveis, constantes e Console.Write x Console.WriteLine

---
+++ C-SHARP/ExemploSintaxeBasica/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Console.WriteLine("Hello, World!");
+
+int variavelDoTipoInteiro = 23;
+float variavelDoTipoFloat = 2.34f;
+bool variavelDoTipoBooleano = true;
+decimal variavelDoTipoDecimal = 45.6m;
+
+const decimal numeroPi = 3.14m;
+Console.WriteLine(numeroPi);
+
+var texto = "Olá, mundo!";
+Console.WriteLine(texto);*/
+
+//--------------------------------------------------------//
+
+/*Console.Write("Olá");
+Console.Write("mundo!");
+
+Console.WriteLine(); 
+
+Console.WriteLine("Olá");
+Console.WriteLine("mundo!");
+
+Console.WriteLine("Olá\nmundo!");
+Console.WriteLine("Olá\tmundo");
+Console.WriteLine("Olá \"mundo\"");
+Console.WriteLine("C:\\documentos\\GitHub");*/
+
